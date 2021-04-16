a2d8f965307f4746d0bcd599bfabe22fa752199e
almehady <almehady@gmail.com>
2021-04-16 21:48:10 +0300

    Test: new regex pattren

--- extract_phone.py
+++ extract_phone.py
@@ -9,9 +9,12 @@ import re
 with open('input.txt') as f:
     lines = f.readlines()
     for mobile in lines:
-        m = re.findall('([0-9]{5}-[0-9]{6})', mobile)
-        n = re.findall('([0-9]{5} [0-9]{6})', mobile)
-        o = re.findall('([0-9]{5}[0-9]{6})', mobile)
+        # p = re.findall('([0-9]{2}[0-9]{5}[0-9]{6})', mobile)
+        m = re.findall('(\d{11,13})', mobile)
+        # m = re.findall('([0-9]{5}-[0-9]{6})', mobile)
+        # n = re.findall('([0-9]{5} [0-9]{6})', mobile)
+        # n = re.findall('([0-9]{5} [0-9]{3} [0-9]{3})', mobile)
+
 
         for i in m:
             x = i.replace('-', '')
@@ -20,18 +23,24 @@ with open('input.txt') as f:
                 f.writelines("\n")
             print(x)
 
-        for j in n:
-            y = j.replace(' ', '')
-            with open('output.txt', 'a') as f:
-                f.writelines(y)
-                f.writelines("\n")
-            print(y)
-
-        for k in o:
-            with open('output.txt', 'a') as f:
-                f.writelines(k)
-                f.writelines("\n")
-            print(k)
+        # for j in n:
+        #     y = j.replace(' ', '')
+        #     with open('output.txt', 'a') as f:
+        #         f.writelines(y)
+        #         f.writelines("\n")
+        #     print(y)
+        #
+        # for k in o:
+        #     with open('output.txt', 'a') as f:
+        #         f.writelines(k)
+        #         f.writelines("\n")
+        #     print(k)
+        #
+        # for l in p:
+        #     with open('output.txt', 'a') as f:
+        #         f.writelines(l)
+        #         f.writelines("\n")
+        #     print(l)
 
     f.close()
     with open('output.txt') as f:
